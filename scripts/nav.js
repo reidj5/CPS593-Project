214c02509f658268e454d1278809598ae1de5667
Reid <reid@sionnachsoftware.com>
2022-02-22 08:25:21 -0500

    Added terms and conditions page

---
+++ scripts/nav.js
@@ -0,0 +1,10 @@
+function createNav() {
+    return (
+    <div class="topnav">
+        <a class="home" href="/index.html">Home</a>
+        <a href="./login.html">Login</a>
+        <a href="./register.html">Register</a>
+        <h2 class="webtitle">Comic Addict</h2>
+    </div>
+)
+    }
